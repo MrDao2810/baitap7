293b2c0caea943e703951d6bf7170da0566fa2b9
create feature <dao.daoquyen833@gmail.com>
2021-11-02 22:25:12 +0700

    done buoi8

--- buoi8/index.js
+++ buoi8/index.js
@@ -82,3 +82,156 @@ function bangCuuChuong() {
         document.getElementById("ketQua6").value += "***" + '\n'
     }
 }
+
+function bai24(){
+    var a =(document.getElementById('numB24').value);
+    a = a.split(",");
+    var n = a.length;
+    var b = [];
+    for(var i =0; i<n; i++)
+    {
+        let dem = 0;
+       for(var j = 1; j <= a[i]; j++)
+       {
+         if(a[i]%j == 0)
+           {
+              dem++;
+           }
+       }
+       if(dem == 2)
+           {
+              b.push(a[i]);
+           }
+    }
+    // console.log(b);
+    document.getElementById('ketquaB24').value = b;
+}
+function bai25(){
+    var a =(document.getElementById('numB25').value);
+    a = a.split(",");
+    var n = a.length;
+    var b = [];
+    for(let i =0; i<n; i++ )
+    {
+        b.push(Math.pow(a[i],2));
+    }
+    document.getElementById('ketquaB25').value = b;
+
+}
+function bai26(){
+    var a =(document.getElementById('numB26').value);
+    a = a.split(",");
+    var n = a.length;
+    var k = document.getElementById('numB26_2').value;
+    var b = [];
+    for(let i =0; i<n; i++ )
+    {
+        if(a[i] == a[i + 1])
+        {
+            document.getElementById('ketquaB26').value = "Khong dk nhap trung gia tri";
+            return;
+        }
+        else{
+            if((parseInt(a[i]) + 1 == k))
+            {
+                b.push(a[i]);
+            }
+            else{
+                if(parseInt(a[i]) - 1 == k)
+                {
+                    b.push(a[i]);
+                }
+            }
+        }
+    }
+    document.getElementById('ketquaB26').value = b;
+}
+function chuanHoa(chuoi){
+   chuoi = chuoi.toLowerCase();
+   chuoi = chuoi.split(" ");
+   var n = chuoi.length;
+   for(var i =0; i<n; i++)
+   {
+       let chuoiTam = chuoi[i].split("");
+       let m = chuoiTam.length;
+       chuoiTam[0] = chuoiTam[0].toUpperCase();
+       chuoi[i] = chuoiTam.join("");
+   }
+   chuoi = chuoi.join(" ");
+   return chuoi;
+}
+function bai27(){
+    var students = [
+        {
+            id: "T3HXX1",
+            firstName: "NgAN",
+            lastName: "duong tHuy"
+        },
+        {
+            id: "T3HXX2",
+            firstName: "hA",
+            lastName: "do tHi Thu"
+        },
+        {
+            id: "T3HXX5",
+            firstName: "miNH",
+            lastName: "Nguyen Nhat"
+        }
+    ];
+    var n = students.length;
+    var b = [];
+    for(let i =0; i<n; i++)
+    {            
+        students[i].firstName = chuanHoa(students[i].firstName);
+        students[i].lastName = chuanHoa(students[i].lastName);
+        if((students[i].firstName).search("a") > 0 || (students[i].firstName).search("A") > 0)
+        {
+            if((students[i].firstName).length >= 3)
+            {
+                 b.push(students[i]);
+            }
+        }
+    }
+    console.log(b);     
+}
+// bai27()
+function bai29(){
+    var students = [
+        {
+            id: "T3HXX1",
+            firstName: "Ngan",
+            lastName: "Duong Thuy"
+        },
+        {
+            id: "T3HXX2",
+            firstName: "Ha",
+            lastName: "Do Thi Thu"
+        },
+        {
+            id: "T3HXX5",
+            firstName: "Minh",
+            lastName: "Nguyen Nhat"
+        }
+    ]
+    console.log("Mang ban dau:");
+    console.log(students);
+    students = students.sort(function(a, b){
+        let x = a.firstName.toLocaleLowerCase();
+        let y = b.firstName.toLocaleLowerCase();
+        if(x > y)
+        { 
+            return 1;
+        }
+        if(x<y)
+        {
+            return -1;
+        }
+    })
+    console.log("Mang sau khi sap xep:");
+    console.log(students);
+    // for(var i =0; i<n; i++)
+    // {
+
+    // 
+}
+bai29()
